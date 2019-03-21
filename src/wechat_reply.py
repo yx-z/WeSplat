b8b501c28d14698924e9a59bc5bb06da773799ee
yzhng223 <yzhng223@illinois.edu>
2019-03-21 15:39:33 -0500

    Send stage images by default

--- src/wechat_reply.py
+++ src/wechat_reply.py
@@ -10,7 +10,6 @@ KEYWORDS_QUERY = ["查询"]
 KEYWORDS_LEAGUE = ["组排", "双排", "四排", "排排", "pp", "wyx", "加班"]
 KEYWORDS_RANKED = ["单排"]
 KEYWORDS_REGULAR = ["涂地", "普通"]
-KEYWORDS_IMAGES = ["图"]
 
 CHINESE_NAME_LEAGUE = "组排"
 CHINESE_NAME_RANKED = "单排"
@@ -38,17 +37,16 @@ def text_reply(msg):
     request_time = time.time()
     schedule = request_schedule(mode, request_time)
 
-    user = msg.user
-    user.send("{}模式: {}, 地图: {}".format(
+    requester = msg.user
+    requester.send("{}模式: {}, 地图: {}".format(
         get_chinese_name(mode), schedule.mode,
         list(map(lambda stage: stage.name, schedule.stages))))
 
-    if any_in(KEYWORDS_IMAGES):
-        for stage in schedule.stages:
-            file_name = stage.name + IMG_EXT
-            if not os.path.isfile(file_name):  # download if not cached
-                download_image(stage.img_url, file_name)
-            user.send_image(file_name)
+    for stage in schedule.stages:
+        file_name = stage.name + IMG_EXT
+        if not os.path.isfile(file_name):  # download if not cached
+            download_image(stage.img_url, file_name)
+        requester.send_image(file_name)
 
 
 def get_chinese_name(mode: str) -> str:
