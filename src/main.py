e9bcee25d6105c9aa25a7de48d16b9f0389cc59d
yzhng223 <yzhng223@illinois.edu>
2019-03-25 13:29:08 -0500

    Fix typo

--- src/main.py
+++ src/main.py
@@ -56,7 +56,7 @@ def reply_salmon_run(requester, request_time: float, request_input: str):
         else:
             remain_message = "还有{}小时开始".format(
                 diff_hours(request_time, run.start_time))
-        requester.send_msg("{rem}, " "地图:{stage}, 武器: {weapon}".format(
+        requester.send_msg("({rem}), 地图: {stage}, 武器: {weapon}".format(
             rem=remain_message,
             stage=dict_get(STAGES, run.stage.name),
             weapon=" ".join(str(s) for s in list(map(
@@ -88,12 +88,12 @@ def reply_battle(requester, mode: str, msg_time: float, request_input: str):
         return
 
     if schedule.start_time <= msg_time <= schedule.end_time:
-        remain_message = " (剩余{}分钟结束) ".format(
+        remain_message = "剩余{}分钟结束".format(
             diff_minutes(msg_time, schedule.end_time))
     else:
-        remain_message = " (还有{}分钟开始) ".format(
+        remain_message = "还有{}分钟开始".format(
             diff_minutes(msg_time, schedule.start_time))
-    requester.send_msg("{mode}: {type}模式{rem}, 地图: {stage}".format(
+    requester.send_msg("{mode}: {type}模式 ({rem}), 地图: {stage}".format(
         mode=dict_get(MODES, mode),
         type=dict_get(BATTLES, schedule.mode),
         rem=remain_message,
